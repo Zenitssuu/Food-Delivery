52c4efd5e454a7e03bd49f807b5e37d05b1c9726
Zenitssuu <sidsaiba2703@gmail.com>
2025-07-21 19:36:05 +0530

    minor updates

--- server/db/index.js
+++ server/db/index.js
@@ -2,15 +2,13 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     // console.log(`MongoDB Connected: ${conn.connection.host}`);
     console.log("Database connected");
     
   } catch (error) {
+    console.log(error);
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
